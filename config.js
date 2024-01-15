36974f196d5a267cd3695353aeaec1c838efac57
guifornagiero <guifornagiero.c@hotmail.com>
2024-01-15 11:14:17 -0300

    feat: delete config

--- config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCy8HjCinE5Sd1kMgx-qxMDVo-PmjvpVCE",
-  authDomain: "foody-6455b.firebaseapp.com",
-  databaseURL: "https://foody-6455b-default-rtdb.firebaseio.com",
-  projectId: "foody-6455b",
-  storageBucket: "foody-6455b.appspot.com",
-  messagingSenderId: "695708420608",
-  appId: "1:695708420608:web:261a05b3d451caa06666e5"
-}
